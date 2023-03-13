52b026f0d916f6721be101d837fc82da1d8f8fb3
aaditimanjalkar <115279767+aaditimanjalkar@users.noreply.github.com>
2023-03-13 19:38:27 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,68 @@
+struct stack 
+{ 
+  int a[5]; 
+  int top; 
+} s; 
+void push() 
+{ 
+    int x; 
+    if (s.top == 4) 
+    printf("Stack is overflow\n"); 
+    else 
+    { 
+        printf("Enter element to push in stack:"); 
+        scanf("%d", &x); 
+        s.top++; 
+        s.a[s.top]=x; 
+    } 
+} 
+void pop() 
+{ 
+    if (s.top == -1) 
+    printf("Stack is underflow"); 
+    else 
+    { 
+        printf("Element poped in the stack is %d \n",s.a[s.top]); 
+        s.top--; 
+    } 
+} 
+void display() 
+{ 
+    if (s.top == -1) 
+    printf("Stack is underflow\n"); 
+    else 
+    { 
+        printf("The element of th stack are \n"); 
+        for(int i = 0; i<= s.top; i++) 
+        printf("%d\n",s.a[i]);
+    } 
+} 
+void destory() 
+{ 
+    s.top = -1; 
+    printf("Stack is destory \n"); 
+} 
+int main(void) 
+{ 
+    int ch; 
+    s.top = -1; 
+    do 
+    { 
+        printf("MENU\n 1.PUSH\n 2.POP\n 3.DISPLAY\n 4.DESTORY\n 5.EXIT\n"); 
+        scanf("%d", &ch); 
+        switch(ch) 
+        { 
+            case 1: push(); 
+            break; 
+            case 2: pop(); 
+            break; 
+            case 3: display(); 
+            break; 
+            case 4: destory(); 
+            break; 
+            case 5: break; 
+            default : printf("Entered a wrong choice"); 
+        } 
+    } 
+    while(ch!=5); 
+} 
